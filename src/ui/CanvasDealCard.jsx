0bd3767647b382bb60e4b7be241c83ced7056693
Lynn Chyi <14299381+lychyi@users.noreply.github.com>
2019-08-10 15:38:00 -0700

    add card ui

--- src/ui/CanvasDealCard.jsx
+++ src/ui/CanvasDealCard.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { canvas } from '@workday/canvas-kit-react';
 import { AccentIcon } from '@workday/canvas-kit-react-icon';
 import { handMoneyIcon } from '@workday/canvas-accent-icons-web';
+
+const Card = styled('div')(
+  {
+    display: 'inline-block',
+    ...canvas.depth[4],
+    border: `${canvas.spacing.xs} solid white`,
+    borderRadius: 8
+  },
+  ({width}) => ({
+    height: 1.4 * width,
+    width: width
+  })
+);
 
 const CardBack = styled('div')(
   {
@@ -10,24 +23,45 @@ const CardBack = styled('div')(
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: canvas.colors.pomegranate500,
-    padding: canvas.spacing.xxs,
-    ...canvas.depth[4],
-    border: `${canvas.spacing.xs} solid white`,
-    borderRadius: 8
+    height: `100%`,
+    width: `100%`
   },
-  ({height, width}) => ({
-    height: height,
-    width: width
-  })
+  
 );
 
-const CanvasDealCard = () => {
+const CardFront = styled('div')(
+  {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: canvas.colors.frenchVanilla200,
+    height: `100%`,
+    width: `100%`
+  }
+);
+
+const CanvasDealCard = (props) => {
+  const [flipped, setFlipped] = useState(false);
+
+  const width = 120;
+
+  const flip = () => {
+    setFlipped(!flipped);
+  }
+
   return (
-    <div>
-      <CardBack height={350} width={250}>
-        <AccentIcon icon={handMoneyIcon} size={180} color={canvas.colors.pomegranate600} />
-      </CardBack>
-    </div>
+    <Card onClick={flip} width={width}>
+      {
+        !flipped ?
+        <CardBack>
+          <AccentIcon icon={handMoneyIcon} size={.8 * width} color={canvas.colors.pomegranate600} />
+        </CardBack>
+        :
+        <CardFront>
+          { props.children }
+        </CardFront>
+      }
+    </Card>
   )
 }
 
