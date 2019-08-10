4a5936186bffd45a04cf1d1ba117b47b9093c98f
Lynn Chyi <14299381+lychyi@users.noreply.github.com>
2019-08-10 14:02:16 -0700

    init canvas deal game board

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ import { Game } from 'boardgame.io/core';
 import SeedDeck from './seedDeck';
 
 export const CanvasDeal = Game({
-  setup: () => ({cells: Array(9).fill(null)}, deck: SeedDeck.run()),
+  setup: () => ({ deck: SeedDeck.run() }),
 
   moves: {
     // draw card/s & draw 5
@@ -12,9 +12,6 @@ export const CanvasDeal = Game({
     // discard card
     // flip wild card
     // move property cards
-    clickCell(G, ctx, id) {
-      G.cells[id] = ctx.currentPlayer;
-    },
   },
   flow: {},
   // playerView: () => ({ /* what the player sees */ }),
