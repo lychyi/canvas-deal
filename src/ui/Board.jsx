73035243c0c38709d62bc6301dd9e9233427a19f
Lynn Chyi <14299381+lychyi@users.noreply.github.com>
2019-08-10 15:16:52 -0700

    nit: formatting

--- src/ui/Board.jsx
+++ src/ui/Board.jsx
@@ -12,9 +12,11 @@ export class CanvasDealBoard extends React.Component {
       <div>
         <h2>Remaining Draw Pile: {G.deck.length}</h2>
         <h2>Your Hand:</h2>
-        {G.players[this.props.playerID].hand.map(item => {
-          return <CanvasDealCard>{item.name}</CanvasDealCard>;
-        })}
+        {
+          G.players[this.props.playerID].hand.map(item => {
+            return <CanvasDealCard>{item.name}</CanvasDealCard>;
+          })
+        }
       </div>
     );
   }
