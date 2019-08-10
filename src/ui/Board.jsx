e0fe3f9cbc5bc926540f186bb1f5ae7b63f4029c
Lynn Chyi <14299381+lychyi@users.noreply.github.com>
2019-08-10 15:14:24 -0700

    rename files

--- src/ui/Board.jsx
+++ src/ui/Board.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { Card, canvas } from '@workday/canvas-kit-react';
-
 import { CanvasDealCard } from './CanvasDealCard';
 
 export class CanvasDealBoard extends React.Component {
@@ -13,10 +11,10 @@ export class CanvasDealBoard extends React.Component {
     return (
       <div>
         <h2>Remaining Draw Pile: {G.deck.length}</h2>
-        <h2>Your Hand : </h2>
-          {G.players[this.props.playerID].hand.map(item => {
-            return <CanvasDealCard>{item.name}</CanvasDealCard>;
-          })}
+        <h2>Your Hand:</h2>
+        {G.players[this.props.playerID].hand.map(item => {
+          return <CanvasDealCard>{item.name}</CanvasDealCard>;
+        })}
       </div>
     );
   }
