c1f7f026dc3bf2d23637843a102980ab68d6a71f
Justin Pante <1861673+jpante@users.noreply.github.com>
2019-08-10 17:03:00 -0700

    Add end turn

--- src/ui/Board.jsx
+++ src/ui/Board.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from '@workday/canvas-kit-react-button';
 
 import { CanvasDealCard } from './CanvasDealCard';
 
 export class CanvasDealBoard extends React.Component {
+
+   static propTypes = {
+     events: PropTypes.any.isRequired,
+   };
+
+  endTurn = () => {
+    this.props.events.endTurn();
+    };
 
   render() {
     const {
@@ -10,19 +20,19 @@ export class CanvasDealBoard extends React.Component {
       ...rest
     } = this.props;
 
-    function onEndClick() {
-    //  this.props.events.endTurn();
-    }
+
 
     return (
       <div>
         <h2>Remaining Draw Pile: {G.deck.length}</h2>
+        <Button onClick={this.endTurn}>End Turn</Button>
         <h2>Your Hand:</h2>
         {
           G.players[this.props.playerID].hand.map(item => {
             return <CanvasDealCard>{item.name}</CanvasDealCard>;
           })
         }
+
       </div>
     );
   }
