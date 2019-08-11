9d52b6b291b3e56971d7624e80f9ea8046f8dd37
Justin Pante <1861673+jpante@users.noreply.github.com>
2019-08-11 05:15:53 -0700

    Add tests for bank, update logic, ad UUID to deck

---
+++ src/HandHelper.js
@@ -0,0 +1,27 @@
+function putCardInBank(player, card) {
+  if (card.type === 'moneyCard' || card.type === 'actionCard') {
+    player.bank.push(card);
+  }else{
+    throw Error('Cannot put this card in the bank');
+  }
+}
+
+function updateBankValue(player){
+  var value = 0;
+  player.bank.forEach(card =>{
+    value += card.value;
+  });
+  player.bankValue = value;
+}
+
+function removeCardFromHand(player, card) {
+  player.hand = player.hand.filter(handCard => handCard.id !== card.id);
+  return card;
+}
+
+const HandHelper = {putCardInBank,
+  removeCardFromHand,
+  updateBankValue,
+};
+
+export default HandHelper;
